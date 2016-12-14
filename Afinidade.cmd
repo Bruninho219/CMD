bc9981423d5f09ce1413ed668b91885af3bc3945
Bruninho219 <brunodarosasilva.brs@hotmail.com>
2016-12-13 22:37:39 -0200

    Create Afinidade.cmd
    Abrir arquivos com afinidade para apenas um processador (CPU 0)

---
+++ Afinidade.cmd
@@ -0,0 +1,9 @@
+@echo
+rem no lugar do 'caminho', coloque o caminho do arquivo +
+rem	nome do arquivo junto com sua extensoão
+
+rem ex.: start /affinity 1 "" "C:\X\Arquivo.exe"
+start /affinity 1 "" "caminho"
+rem O aplicativo vai usar apenar um processador (CPU 0)
+
+exit
